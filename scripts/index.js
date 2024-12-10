347049cb722ffedd3c612cec6ef884c404a68349
Andrey Chernykh <andreychernykh04@gmail.com>
2024-12-10 21:14:15 +0300

    feat: implement card creation and appending functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+/**
+ * @param {string} name
+ * @param {string} link
+ * @returns {Node}
+ */
+function createCard({name, link}) {
+  const template = document.querySelector("#card-template").content;
+  const card = template.cloneNode(true);
+  const image = card.querySelector(".card__image");
+  const description = card.querySelector(".card__description");
 
-// @todo: DOM узлы
+  image.src = link;
+  image.alt = `Изображение "${name}"`;
+  description.textContent = name;
 
-// @todo: Функция создания карточки
+  return card;
+}
 
-// @todo: Функция удаления карточки
+/**
+ * @param {Node} card
+ */
+function appendCard(card) {
+  const places = document.querySelector(".places__list");
 
-// @todo: Вывести карточки на страницу
+  places.append(card);
+}
+
+initialCards.map(createCard).forEach(appendCard);
